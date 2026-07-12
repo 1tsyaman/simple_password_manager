9e97244da28452b99a5e678d9482ad4bbad36293
1tsYaman <y-faour@hotmail.com>
2026-07-12 18:21:50 +0200

    Added new totp_config member to the class Entry and adjusted creating/getter/setter methods.
    Signed-off-by: 1tsYaman <y-faour@hotmail.com>

--- core/entry.py
+++ core/entry.py
@@ -12,8 +12,12 @@ class Entry:
 		obj = {
 			"website":	self.website,
 			"username":	self.username,
-			"description":	self.description
+			"description":	self.description,
+			"totp_config":	{}
 		}
+
+		if self.totp_config is not None:
+			obj["totp_config"] = self.totp_config.to_json()
 
 		return obj
 
@@ -26,6 +30,9 @@ class Entry:
 	def get_description(self: Entry) -> str:
 		return self.description
 	
+	def get_totp_config(self: Entry) -> TOTP_Config | None:
+		return self.totp_config
+	
 	def set_website(self: Entry, website: str):
 		self.website = website
 	
@@ -35,8 +42,8 @@ class Entry:
 	def set_description(self: Entry, description: str):
 		self.description = description
 	
-	def set_totp_config(self: Entry, uri: str):
-		self.totp_config = TOTP_Config.from_uri(uri)
+	def set_totp_config(self: Entry, totp_config: TOTP_Config | None):
+		self.totp_config = totp_config
 
 	"""
 		This function ignores the description
@@ -54,18 +61,34 @@ class Entry:
 	def to_string_with_desc(self: Entry) -> str:
 		return f"Website: {self.website}\nUsername: {self.username}\nDescription: {self.description}"
 
-
 	@staticmethod
-	def create_entry(website: str, username: str, description="", totp_uri=""):
+	def create_entry(website: str, username: str, description="", totp_config: TOTP_Config | None = None):
 		entry = Entry()
 
 		entry.website		= website
 		entry.username		= username
 		entry.description	= description
-		entry.totp_config	= TOTP_Config.from_uri(totp_uri)
+		entry.totp_config	= totp_config
 
 		return entry
 
+	"""
+		entry: dict[str, str | dict[str, str]]: key=config has value: dict[str, str]
+	"""
 	@staticmethod
-	def from_json(entry: dict[str, str]):
-		return Entry.create_entry(entry["website"], entry["username"], entry["description"])
+	def from_json(entry: dict) -> Entry | None:
+		try:
+			website		= entry["website"]
+			username	= entry["username"]
+			description	= entry["description"]
+
+			if not all(isinstance(value, str) for value in [website, username, description]):
+				return None
+
+			config = TOTP_Config.from_json(entry["totp_config"])
+			
+			return Entry.create_entry(website=website, username=username,
+			     				description=description, totp_config=config)
+
+		except (KeyError, TypeError):
+			return None
