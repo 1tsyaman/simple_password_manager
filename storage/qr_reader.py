07f8dec509d0e6e44a7600b767cccb4c04c2b552
Yaman Faour <yaman.faour@rwth-aachen.de>
2026-09-03 01:20:37 +0200

    Fixed android compatibility issues. Now, qr_reader.py is cross-platform

--- storage/qr_reader.py
+++ storage/qr_reader.py
@@ -1,5 +1,4 @@
-from PIL import Image
-from zxingcpp import read_barcode
+from kivy.utils import platform
 
 from core.errors import (
 	ImageOpenError,
@@ -7,18 +6,39 @@ from core.errors import (
 	log
 )
 
+
+if platform == "android":
+	from jnius import autoclass
+
+	BitmapFactory = autoclass("android.graphics.BitmapFactory")
+	Rect = autoclass("android.graphics.Rect")
+	BarcodeReader = autoclass("zxingcpp.BarcodeReader")
+
+	barcode_reader = BarcodeReader()
+
+else:
+	from PIL import Image
+	from zxingcpp import read_barcode
+
+
 """
 	@raises:
 		- ImageOpenError
 		- QRDecodeError
-
 """
 def read_qr_code(image_path: str) -> str:
+	if platform == "android":
+		return _read_qr_code_android(image_path)
+
+	return _read_qr_code_desktop(image_path)
+
+
+def _read_qr_code_desktop(image_path: str) -> str:
 	try:
 		image = Image.open(image_path)
 	except Exception as e:
 		log(
-			message=f"Something went wrong while openning the image {image_path}",
+			message=f"Something went wrong while opening the image {image_path}",
 			error=e
 		)
 
@@ -28,5 +48,38 @@ def read_qr_code(image_path: str) -> str:
 
 	if result is not None:
 		return result.text
+
+	raise QRDecodeError
+
+
+def _read_qr_code_android(image_path: str) -> str:
+	try:
+		bitmap = BitmapFactory.decodeFile(image_path)
+
+		if bitmap is None:
+			raise ValueError("BitmapFactory could not decode image")
+	except Exception as e:
+		log(
+			message=f"Something went wrong while opening the image {image_path}",
+			error=e
+		)
+
+		raise ImageOpenError
+
+	rect = Rect(
+		0,
+		0,
+		bitmap.getWidth(),
+		bitmap.getHeight()
+	)
+
+	results = barcode_reader.read(
+		bitmap,
+		rect,
+		0
+	)
+
+	if results.size() > 0:
+		return results.get(0).getText()
 
 	raise QRDecodeError
